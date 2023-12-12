b4fd2ca265cd14a15f4bd04fd828926966e0489e
Aditya <adijaiswal0307@gmail.com>
2023-12-12 18:23:11 +0530

    1st

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TechnologyComponent } from './technology/technology.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tech', component: TechnologyComponent },
+  { path: 'home', component: HomeComponent },
+  {path:'about',component:AboutComponent},
+  {path:'career',component:CareersComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { AboutComponent } from './about/about.component';
+import { CareersComponent } from './careers/careers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    TechnologyComponent,
+    AboutComponent,
+    CareersComponent
   ],
   imports: [
     BrowserModule,
